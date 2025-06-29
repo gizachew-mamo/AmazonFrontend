09efa709f6e974ffa8037330d31d414f5e04a682
gizachew-mamo <Gizeworknewu@gmail.com>
2025-06-29 07:40:33 -0700

    backend url added

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase function
-  baseURL: "http://127.0.0.1:5001/clone2025-138e6/us-central1/api",
+  
+  baseURL: "https://amazonbackend-ubnu.onrender.com",
   responseType: "json",
 });
 
